abbdde4a410fb07c0ebea69d57ee47fafd26367e
Strokkur24 <strokkur.24@gmail.com>
2024-11-01 18:24:20 +0100

    Initial Commit (IntelliJ)

---
+++ src/main/kotlin/net/strokkur/craftAttackReloaded/CraftAttackReloaded.kt
@@ -0,0 +1,14 @@
+package net.strokkur.craftAttackReloaded
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class CraftAttackReloaded: JavaPlugin() {
+
+    override fun onEnable() {
+
+    }
+
+    override fun onDisable() {
+
+    }
+}
